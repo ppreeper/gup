afa70773f5557571f1f82c13f3f20c7b1f4f1756
Peter Preeper <ppreeper@gmail.com>
2023-07-02 17:24:15 -0600

    add install go

---
+++ src/install_go_command.sh
@@ -0,0 +1,29 @@
+CMD="go"
+REPO="https://github.com/golang/go"
+DLREPO="https://dl.google.com/go"
+vers=$(git ls-remote --tags ${REPO} | grep "refs/tags/go.*[0-9]$" | grep -v -e rc -e alpha -e beta | awk '{print $2}' | sed 's/refs\/tags\///' | sort -V | uniq | tail -1)
+
+IDIR=/usr/lib
+BDIR=/usr/bin
+
+function download(){
+    echo "download $1 version"
+    echo "Installing $vers"
+    FN=$vers.linux-amd64.tar.gz
+    rm -f /tmp/${FN}
+    wget -qc ${DLREPO}/${FN} -O /tmp/${FN}
+    sudo ls ${IDIR}/go/bin 2>/dev/null | sudo xargs -I {} rm -f ${BDIR}/{}
+    sudo rm -rf ${IDIR}/go
+    sudo tar axf /tmp/${FN} -C ${IDIR}
+    sudo ls ${IDIR}/go/bin | sudo xargs -I {} ln -sf ${IDIR}/go/bin/{} ${BDIR}/{}
+    rm -f /tmp/${FN}
+}
+
+if [ -z $(which ${CMD}) ]; then
+    download new
+else
+    APP=$(which ${CMD})
+    APPVER=$(${APP} version | awk '{print $3}')
+    [ "${APPVER}" = "${vers}" ] && echo "${CMD} version is current" || download $vers
+fi
+

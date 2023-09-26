b5df9fc7d8a22840b47e9833ba0de083b05fb181
Peter Preeper <ppreeper@gmail.com>
2023-09-26 00:42:51 -0600

    add litexl

---
+++ src/install_litexl_command.sh
@@ -0,0 +1,16 @@
+APP="litexl"
+REPO="https://github.com/lite-xl/lite-xl"
+vers=$(git ls-remote --tags ${REPO} | grep "refs/tags.*[0-9]$" | awk '{print $2}' | sed 's/refs\/tags\///' | grep "^v" | sort -V | uniq | tail -1)
+
+BDIR=${HOME}/.local/bin
+
+function download() {
+    echo "download $1 version"
+    echo "installing ${vers}"
+    mkdir -p ${BDIR}
+    FN=LiteXL-${vers}-addons-x86_64.AppImage
+    wget -qc ${REPO}/releases/download/${vers}/LiteXL-v2.1.1-addons-x86_64.AppImage -O ${BDIR}/litexl
+    chmod +x ${BDIR}/litexl
+}
+
+download ${vers}

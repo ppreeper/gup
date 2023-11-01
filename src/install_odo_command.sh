9baaf2abb799b101b60e663eb554b158be1c4c4e
Peter Preeper <ppreeper@gmail.com>
2023-10-31 22:20:06 -0600

    [ADD] add the odo command

---
+++ src/install_odo_command.sh
@@ -0,0 +1,12 @@
+REPO="https://raw.githubusercontent.com/ppreeper/odo"
+USERIDNUMBER=$(grep $(whoami) /etc/passwd | awk -F":" '{print $3}')
+
+if [ ${USERIDNUMBER} == 0 ]; then
+  IDIR="/usr/local/bin"
+  sudo wget -q ${REPO}/main/odo -O ${IDIR}/odo
+  chmod +x ${IDIR}/odo
+else
+  IDIR="${HOME}/.local/bin"
+  wget -q ${REPO}/main/odo -O ${IDIR}/odo
+  chmod +x ${IDIR}/odo
+fi

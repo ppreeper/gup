ab3dbd8e1ba171a4ed207cae0d273144c4689f0b
Peter Preeper <ppreeper@gmail.com>
2024-06-10 11:56:08 -0600

    add ruff python linter

---
+++ src/install_ruff_command.sh
@@ -0,0 +1,24 @@
+APP="ruff"
+REPO="https://github.com/astral-sh/ruff"
+vers=$(git ls-remote --tags ${REPO} | grep "refs/tags.*[0-9]$" | grep -v -e "rc" -e "alpha" -e "beta" | awk '{print $2}' | sed 's/refs\/tags\///g' | sort -V | uniq | tail -1)
+
+BDIR=/usr/local/bin
+
+function download() {
+    echo "download $1 version"
+    echo "installing ${vers}"
+    V=$(echo $vers | sed 's/^v//')
+    FN=${APP}-${V}-x86_64-unknown-linux-gnu.tar.gz
+    rm -f /tmp/${FN}
+    wget -qc ${REPO}/releases/download/${vers}/${FN} -O /tmp/${FN}
+    sudo tar -axf /tmp/${FN} -C ${BDIR}
+    rm -f /tmp/${FN}
+}
+
+if [ -z $(which ${APP}) ]; then
+    download new
+else
+    APPBIN=$(which ${APP})
+    APPVER=$(${APPBIN} version | awk '{print "v"$2}')
+    [ "${APPVER}" = "${vers}" ] && echo "${APP} version is current" || download ${vers}
+fi

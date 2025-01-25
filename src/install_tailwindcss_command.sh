b31811878069861bbfdf8cf79004a742dcbecba2
Peter Preeper <ppreeper@gmail.com>
2025-01-24 20:01:39 -0700

    tailwindcss v4

--- src/install_tailwindcss_command.sh
+++ src/install_tailwindcss_command.sh
@@ -1,6 +1,6 @@
 APP="tailwindcss"
 REPO="https://github.com/tailwindlabs/tailwindcss"
-vers=$(git ls-remote --tags ${REPO} | grep "refs/tags.*[0-9]$" | awk '{print $2}' | sed 's/refs\/tags\///' | sort -V | uniq | tail -1)
+vers=$(git ls-remote --tags ${REPO} | grep "refs/tags.*[0-9]$" | grep -v -e alpha -e beta -e canary | awk '{print $2}' | sed 's/refs\/tags\///' | sort -V | uniq | tail -1)
 
 if [ $(id -u) == 0 ]; then
   BDIR="/usr/local/bin"
@@ -12,7 +12,7 @@ function download() {
     echo "download $1 version"
     echo "installing ${vers}"
     FN=${APP}-linux-x64
-    wget -qc ${REPO}/releases/download/${vers}/${FN} -O ${BDIR}/${APP}
+    wget -q ${REPO}/releases/download/${vers}/${FN} -O ${BDIR}/${APP}
     chmod +x ${BDIR}/${APP}
 }
 
@@ -20,6 +20,6 @@ if [ -z $(which ${APP}) ]; then
     download new
 else
     APPBIN=$(which ${APP})
-    APPVER=$(${APPBIN} --help 2>&1 | grep ^${APP} | awk '{print $2}')
+    APPVER=$(${APPBIN} --help 2>&1 | grep "${APP}.*v" | awk '{print $3}')
     [ "${APPVER}" = "${vers}" ] && echo "${APP} version is current" || download ${vers}
 fi

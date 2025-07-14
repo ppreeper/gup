adb09804b19dbbf89d2da12bc71ee63ca0b477aa
Peter Preeper <ppreeper@gmail.com>
2025-07-13 22:34:10 -0600

    add rustup update

--- src/install_rust_command.sh
+++ src/install_rust_command.sh
@@ -1,1 +1,10 @@
-wget -qO- https://sh.rustup.rs | sh
+APPBIN=$(which rustup 2>/dev/null || echo "")
+
+if [ -z "${APPBIN}" ]; then
+    echo "Installing Rust"
+    wget -qO- https://sh.rustup.rs | sh
+else
+    echo "Upgrading Rust"
+    "${APPBIN}" update
+    exit 0
+fi

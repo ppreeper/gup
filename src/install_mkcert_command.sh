6713bf574b8e8a738069fb0435dba6cc0b3c9ea6
Peter Preeper <ppreeper@gmail.com>
2025-07-21 17:07:21 -0600

    update mkcert

--- src/install_mkcert_command.sh
+++ src/install_mkcert_command.sh
@@ -1,5 +1,6 @@
 APP="mkcert"
 
-sudo apt install libnss3-tools
-sudo wget -O "/usr/local/bin/${APP}" "https://dl.filippo.io/mkcert/latest?for=linux/amd64"
-sudo "/usr/local/bin/${APP}"
+sudo apt update -y && \
+sudo apt install -y libnss3-tools && \
+sudo wget -O "/usr/local/bin/${APP}" "https://dl.filippo.io/mkcert/latest?for=linux/amd64" && \
+sudo chmod +x "/usr/local/bin/${APP}"

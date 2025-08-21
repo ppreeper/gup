4bdf589f37e98a4fa7fdb99370fc16c1a50a5ed1
Peter Preeper <ppreeper@gmail.com>
2025-08-21 10:27:34 -0600

    add node_exporter

---
+++ src/install_node_exporter_command.sh
@@ -0,0 +1,27 @@
+APP="node_exporter"
+REPO="https://github.com/prometheus/node_exporter"
+vers=$(git ls-remote --tags ${REPO} | grep "refs/tags.*[0-9]$" | grep -v -e rc -e alpha -e beta | awk '{print $2}' | sed 's/refs\/tags\///' | sort -V | uniq | tail -1)
+
+BDIR="/usr/local/bin"
+
+download() {
+    echo "download $1 version"
+    echo "installing ${vers}"
+    V=$(echo "$vers" | sed 's/v//')
+    FN=${APP}-${V}.linux-amd64.tar.gz
+    rm -f /tmp/"${FN}"
+    wget -qc ${REPO}/releases/download/"${vers}"/"${FN}" -O /tmp/"${FN}"
+    rm -f ${BDIR}/${APP}
+    mkdir -p /tmp/${APP}_"${vers}"
+    tar axf /tmp/"${FN}" -C /tmp/${APP}_"${vers}" --strip-components=1
+    sudo install /tmp/${APP}_"${vers}"/${APP} ${BDIR}/${APP}
+    rm -rf /tmp/${APP}_"${vers}" /tmp/"${FN}"
+}
+
+if [ -z "$(which ${APP})" ]; then
+    download new
+else
+    APPBIN=$(which ${APP})
+    APPVER=$(${APPBIN} --version 2>&1 | grep -i ^${APP} | awk '{print $3}')
+    [ "${APPVER}" = "$(echo "$vers" | sed 's/^v//')" ] && echo "${CMD} version is current" || download "${vers}"
+fi

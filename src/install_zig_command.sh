e8a024950e3c244d6396de27145b2b2414283424
Peter Preeper <ppreeper@gmail.com>
2025-10-30 13:05:12 -0600

    zig use official source

--- src/install_zig_command.sh
+++ src/install_zig_command.sh
@@ -1,11 +1,8 @@
 APP="zig"
-REPO="ziglang/zig"
-RURL="https://api.github.com/repos/${REPO}/releases/latest"
-vers=$(wget -qO- "${RURL}" | jq .tag_name | tr -d '"' | tr -d 'v')
-
-DLREPO="https://ziglang.org/download"
-FN="zig-x86_64-linux-$vers.tar.xz"
-DL="${DLREPO}/$vers/${FN}"
+vers=$(wget -qO- https://ziglang.org/download/index.json | jq 'to_entries | map(select(.key !="master")) | .[] | .key' | tr -d '"' | sort -V | uniq | tail -1)
+qstring=".\"${vers}\".\"x86_64-linux\".\"tarball\""
+DL=$(wget -qO- https://ziglang.org/download/index.json | jq "${qstring}" | tr -d '"')
+FN=$(basename "${DL}")
 
 IDIR=/usr/local/lib
 BDIR=/usr/local/bin

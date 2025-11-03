c233bdc7c01cd1a0658794517d737a941e24cb95
Peter Preeper <ppreeper@gmail.com>
2025-11-03 16:07:24 -0700

    change task from go install to github.com install

--- src/install_task_command.sh
+++ src/install_task_command.sh
@@ -1,1 +1,37 @@
-go install github.com/go-task/task/v3/cmd/task@latest
+# go install github.com/go-task/task/v3/cmd/task@latest
+APP="task"
+REPO="go-task/task"
+RURL="https://api.github.com/repos/${REPO}/releases/latest"
+vers=$(wget -qO- "${RURL}" | jq .tag_name | tr -d '"' | tr -d 'v')
+DL=$(wget -qO- "${RURL}" | jq '.assets[] | select(.name | contains("linux_amd64.tar.gz")) | .browser_download_url' | tr -d '"')
+FN=$(wget -qO- "${RURL}" | jq '.assets[] | select(.name | contains("linux_amd64.tar.gz")) | .name' | tr -d '"')
+
+function download() {
+    echo "download $1 version"
+    echo "installing ${vers}"
+    rm -rf /tmp/"${FN}" /tmp/"${APP}_${vers}"
+    wget -qc "${DL}" -O /tmp/"${FN}"
+    mkdir -p /tmp/"${APP}_${vers}"
+    tar -axf /tmp/"${FN}" -C /tmp/"${APP}_${vers}"
+    if [ "$(id -u)" == 0 ]; then
+        BDIR="/usr/local/bin"
+        sudo rm -f "${BDIR}"/${APP}
+        sudo install /tmp/"${APP}_${vers}/${APP}" "${BDIR}/${APP}"
+    else
+        BDIR="${HOME}/.local/bin"
+        rm -f "${BDIR}"/${APP}
+        install /tmp/"${APP}_${vers}/${APP}" "${BDIR}/${APP}"
+    fi
+    rm -rf /tmp/"${FN}" /tmp/"${APP}_${vers}"
+}
+
+if [ -z "$(which ${APP})" ]; then
+    download new
+else
+    APPVER=$($(which "${APP}") --version 2>&1 | tr -d 'v')
+    if [ "${APPVER}" = "${vers}" ]; then
+        echo "${APP} version is current"
+    else
+        download "${vers}"
+    fi
+fi

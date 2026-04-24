087e0b6dae3c84e0839f2b163888a31da536b0ad
Peter Preeper <ppreeper@gmail.com>
2026-04-24 10:25:06 -0600

    add stylua helper

--- src/install_goimports_command.sh
+++ src/install_goimports_command.sh
@@ -2,7 +2,6 @@ set -euo pipefail
 
 APP="goimports"
 REPO="golang/tools"
-vers=$(gup_get_latest_release "${REPO}")
 
 gup_ensure_go
-go install golang.org/x/tools/cmd/goimports@"${vers}"
+go install golang.org/x/tools/cmd/goimports@latest
---
+++ src/install_stylua_command.sh
@@ -0,0 +1,31 @@
+set -euo pipefail
+
+APP="stylua"
+REPO="JohnnyMorganz/StyLua"
+BDIR="${HOME}/.local/bin"
+gup_fetch_release "${REPO}" 'contains("linux-x86_64.zip")'
+
+download() {
+    echo "download $1 version"
+    echo "installing ${GUP_REL_VERSION}"
+
+    local tmp_dir
+    tmp_dir=$(gup_mktemp_dir)
+    trap 'rm -rf "${tmp_dir}"' RETURN
+
+    gup_download "${GUP_REL_DL}" "${tmp_dir}/${GUP_REL_FN}"
+    unzip -o "${tmp_dir}/${GUP_REL_FN}" -d "${tmp_dir}"
+    mv "${tmp_dir}/stylua" "${BDIR}/stylua"
+    chmod +x "${BDIR}/stylua"
+}
+
+if [ -z "$(command -v "${APP}")" ]; then
+    download new
+else
+    APPVER=$($(command -v "${APP}") -V 2>&1 | grep -e "^stylua" | awk '{print $2}')
+    if [ "${APPVER}" = "${GUP_REL_VERSION}" ]; then
+        echo "${APP} version is current"
+    else
+        download "${GUP_REL_VERSION}"
+    fi
+fi
